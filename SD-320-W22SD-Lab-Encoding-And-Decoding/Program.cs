0fbe5367c58339cc4b950c4b73e024c6f4851fc1
Michael John Santos <michaeljohnsantos@student.mitt.ca>
2022-10-02 21:54:38 -0500

    Add project files.

---
+++ SD-320-W22SD-Lab-Encoding-And-Decoding/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+
+void encode(string inputPath, string outputPath)
+{
+    string encodedString = "Text to be encoded";
+    Encoding ascii = Encoding.ASCII;
+    byte[] asciiBytes = ascii.GetBytes(encodedString);
+
+    for (int i = 0; i < asciiBytes.Length; i++)
+    {
+        Console.WriteLine(asciiBytes[i]);
+    }
+}
+
+void decode(string inputPath, string outputPath)
+{
+    string encodedString = "This has been decoded";
+    Encoding utf8 = Encoding.UTF8;
+    byte[] utf8Bytes = utf8.GetBytes(inputPath);
+    string decodedText = UTF8Encoding.UTF8.GetString(utf8Bytes);
+
+    for (int i = 0; i < utf8Bytes.Length; i++)
+    {
+        Console.WriteLine(utf8Bytes[i]);
+    }
+    Console.WriteLine(encodedString);
+}
+
+encode(@"C:\Users\hello\OneDrive\Desktop\Sample.txt", @"C:\Users\hello\OneDrive\Desktop\Notes2");
+decode(@"C:\Users\hello\OneDrive\Desktop\Sample.txt", @"C:\Users\hello\OneDrive\Desktop\Notes2");
